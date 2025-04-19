a5123e8e602e0b9794c9a7f1630cd9560f2404cb
kuotsanhsu <learningstud@gmail.com>
2025-04-19 17:59:44 +0800

    move all 4 syscall wrappers from liburing into liburing.h

--- io_uring/hello.c
+++ io_uring/hello.c
@@ -1,36 +1,9 @@
+#include "liburing.h"
 #include <err.h>
 #include <error.h>
-#include <linux/io_uring.h>
-#include <signal.h>
 #include <stdatomic.h>
 #include <string.h>
 #include <sys/mman.h>
-#include <sys/syscall.h>
-#include <unistd.h>
-
-// https://man7.org/linux/man-pages/man2/io_uring_setup.2.html
-int io_uring_setup(unsigned entries, struct io_uring_params *p) {
-  return syscall(__NR_io_uring_setup, entries, p);
-}
-
-// https://man7.org/linux/man-pages/man2/io_uring_enter.2.html
-int io_uring_enter2(unsigned fd, unsigned to_submit, unsigned min_complete,
-                    unsigned flags, sigset_t *sig, size_t sz) {
-  return syscall(__NR_io_uring_enter, fd, to_submit, min_complete, flags, sig,
-                 sz);
-}
-
-// https://man7.org/linux/man-pages/man2/io_uring_enter.2.html
-int io_uring_enter(unsigned fd, unsigned to_submit, unsigned min_complete,
-                   unsigned flags, sigset_t *sig) {
-  return io_uring_enter2(fd, to_submit, min_complete, flags, sig, _NSIG / 8);
-}
-
-// https://man7.org/linux/man-pages/man2/io_uring_register.2.html
-int io_uring_register(unsigned fd, unsigned opcode, const void *arg,
-                      unsigned nr_args) {
-  return syscall(__NR_io_uring_register, fd, opcode, arg, nr_args);
-}
 
 #define QUEUE_DEPTH 1
 #define BLOCK_SZ 1024
